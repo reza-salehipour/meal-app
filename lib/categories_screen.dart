f24a9d895b8e46e4f5c484ecf223167222874495
Reza Salehipour <rez.salehipour@gmail.com>
2023-01-15 12:13:45 +0000

    working with gridview.count and mapping categories

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,18 +1,33 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+
+import './dummy_data.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisExtent: 20),
+    return Scaffold(
+      appBar: AppBar(title: const Text('DeliMeal')),
+      body: GridView.count(
+        padding: const EdgeInsets.all(25),
+        crossAxisCount: 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+        childAspectRatio: 3 / 2,
+
+        // gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        //     maxCrossAxisExtent: 200,
+        //     childAspectRatio: 3 / 2,
+        //     crossAxisSpacing: 20,
+        //     mainAxisExtent: 20),
+        children: DUMMY_CATEGORIES
+            .map((categoryData) => CategoryItem(
+                  title: categoryData.title,
+                  color: categoryData.color,
+                ))
+            .toList(),
+      ),
     );
   }
 }

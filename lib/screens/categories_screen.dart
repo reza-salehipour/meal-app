55664591bdaa99dd45caeda5a1eb728a8ceba9c5
Reza Salehipour <rez.salehipour@gmail.com>
2023-01-15 19:49:21 +0000

    fixing rest of widgets

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+import '../widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('DeliMeal')),
+      body: GridView.count(
+        padding: const EdgeInsets.all(25),
+        crossAxisCount: 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+        childAspectRatio: 3 / 2,
+
+        // gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        //     maxCrossAxisExtent: 200,
+        //     childAspectRatio: 3 / 2,
+        //     crossAxisSpacing: 20,
+        //     mainAxisExtent: 20),
+        children: DUMMY_CATEGORIES
+            .map((categoryData) => CategoryItem(
+                  id: categoryData.id,
+                  title: categoryData.title,
+                  color: categoryData.color,
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // const CategoryMealsScreen(
+  //     {required this.categoryId, required this.categoryTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+
+    final String? categoryTitle = routeArgs['title'];
+    final String? categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle!),
+      ),
+      // ignore: prefer_const_constructors
+      body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                duration: categoryMeals[index].duration,
+                affordability: categoryMeals[index].affordability,
+                complexity: categoryMeals[index].complexity);
+          },
+          itemCount: categoryMeals.length),
+    );
+  }
+}

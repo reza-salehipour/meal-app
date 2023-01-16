2f71d0e03da5ca39e982978d28139ea010658561
Reza Salehipour <rez.salehipour@gmail.com>
2023-01-16 14:44:51 +0000

    remove appbar data

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -7,28 +7,25 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('DeliMeal')),
-      body: GridView.count(
-        padding: const EdgeInsets.all(25),
-        crossAxisCount: 2,
-        mainAxisSpacing: 20,
-        crossAxisSpacing: 20,
-        childAspectRatio: 3 / 2,
+    return GridView.count(
+      padding: const EdgeInsets.all(25),
+      crossAxisCount: 2,
+      mainAxisSpacing: 20,
+      crossAxisSpacing: 20,
+      childAspectRatio: 3 / 2,
 
-        // gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        //     maxCrossAxisExtent: 200,
-        //     childAspectRatio: 3 / 2,
-        //     crossAxisSpacing: 20,
-        //     mainAxisExtent: 20),
-        children: DUMMY_CATEGORIES
-            .map((categoryData) => CategoryItem(
-                  id: categoryData.id,
-                  title: categoryData.title,
-                  color: categoryData.color,
-                ))
-            .toList(),
-      ),
+      // gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+      //     maxCrossAxisExtent: 200,
+      //     childAspectRatio: 3 / 2,
+      //     crossAxisSpacing: 20,
+      //     mainAxisExtent: 20),
+      children: DUMMY_CATEGORIES
+          .map((categoryData) => CategoryItem(
+                id: categoryData.id,
+                title: categoryData.title,
+                color: categoryData.color,
+              ))
+          .toList(),
     );
   }
 }

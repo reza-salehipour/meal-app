28ff88f562b7d6c074e844a3b0c12362832bf2f3
Reza Salehipour <rez.salehipour@gmail.com>
2023-01-16 05:11:32 +0000

    working with onGenerateRoute&onUnknownRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'screens/category_meals_screen.dart';
 
+import 'screens/meal_detail_screen.dart';
+import 'screens/category_meals_screen.dart';
 import 'screens/categories_screen.dart';
 
 void main() {
@@ -31,7 +32,15 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (ctx) => CategoriesScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen()
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen()
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        // return MaterialPageRoute(builder: (ctx)=> CategoriesScreen())
+      },
+      onUnknownRoute: (settings) {
+        // return MaterialPageRoute(builder: (ctx)=> CategoriesScreen())
       },
     );
   }

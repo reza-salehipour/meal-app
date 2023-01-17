15a2053029ad6ff96187527ab77027bdeba0dcc2
Reza Salehipour <rez.salehipour@gmail.com>
2023-01-17 02:13:16 +0000

    add removeItem to constructor and add navigations

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -12,6 +12,7 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
+  final Function removeItem;
 
   MealItem(
       {required this.id,
@@ -19,7 +20,8 @@ class MealItem extends StatelessWidget {
       required this.imageUrl,
       required this.duration,
       required this.affordability,
-      required this.complexity});
+      required this.complexity,
+      required this.removeItem});
 
   String get complexityText {
     switch (complexity) {
@@ -48,7 +50,13 @@ class MealItem extends StatelessWidget {
   }
 
   void selectMeal(BuildContext context) {
-    Navigator.of(context).pushNamed(MealDetailScreen.routeName, arguments: id);
+    Navigator.of(context)
+        .pushNamed(MealDetailScreen.routeName, arguments: id)
+        .then((result) {
+      if (result != null) {
+        removeItem(result);
+      }
+    });
   }
 
   @override
